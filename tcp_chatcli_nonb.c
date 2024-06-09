8c480bbac0ffa00667a6f3953b353ef22b574f96
Jinhozino1 <skvofj014@naver.com>
2024-06-09 16:49:21 +0900

    polling system

--- tcp_chatcli_nonb.c
+++ tcp_chatcli_nonb.c
@@ -2,7 +2,7 @@
 // 파일명 : tcp_chatcli_nonb.c 
 // 기   능 : 넌블록 모드의 채팅 서버
 // 컴파일 : gcc -o tcp_chatcli_nonb tcp_chatcli_nonb.c
-// 사용법 : tcp_chatcli_nonb 192.168.64.3 4001
+// 사용법 : tcp_chatcli_nonb 192.168.64.3 4001 [사용자 지정 이름]
 //-----------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]) {
 	fd_set read_fds;// 읽기를 감지할 fd_set 구조체
 
 	if(argc != 4) {
-		printf("사용법 : %s server_ip port name\n", argv[0]);
+		printf("사용법 : %s server_ip port customName\n", argv[0]);
 		exit(0);
 	}
 
